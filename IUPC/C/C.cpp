aeba0538ad67ce3c8bc97ec21500f3b0e89e25fc
seonpilKim <kimsunpil123@naver.com>
2021-01-14 20:30:04 +0900

    [C] 앨범정리 : 예제2, 3 - 반례

--- IUPC/C/C.cpp
+++ IUPC/C/C.cpp
@@ -2,21 +2,34 @@
 #include <algorithm>
 #include <vector>
 
+using namespace std;
+
 class Album {
 public:
-    Album* child;
+    vector<Album*> children;
     Album* parent;
     vector<string> pictures;
     string name;
+    int size_pic;
+    int size_alb;
 
     Album(string name){
         this->name = name;
         this->parent = NULL;
-        this->child = NULL;
+        this->size_alb = 0;
+        this->size_pic = 0;
     }
 
     void Insert(string name) {
-        if (Search(name) == EOF) this->pictures.push_back(name);
+        if (this->pictures.size() == 0) {
+            this->pictures.push_back(name);
+            this->size_pic++;
+        }
+        else if (Search(name) == EOF) {
+            this->pictures.push_back(name);
+            this->size_pic++;
+            sort(this->pictures.begin(), this->pictures.end());
+        }
         else cout << "duplicated photo name\n";     
     }
 
@@ -24,22 +37,24 @@ public:
         int n = 0;
         if (this->pictures.size() < 1) return;
         else if (name == "-1") {
-            sort(this->pictures.begin(), this->pictures.end());
             this->pictures.erase(this->pictures.begin());
+            this->size_pic--;
             n++;
         }
         else if (name == "0") {
             n = this->pictures.size();
             this->pictures.clear();
+            this->size_pic = 0;
         }
         else if (name == "1") {
-            sort(this->pictures.begin(), this->pictures.end());
             this->pictures.pop_back();
+            this->size_pic--;
             n++;
         }
         else {
             int idx = Search(name);
-            if (idx != EOF) this->pictures.erase(this->pictures.begin() + idx);    
+            if (idx != EOF) this->pictures.erase(this->pictures.begin() + idx);
+            this->size_pic--;
             n++;
         }
         cout << n << "\n";
@@ -47,23 +62,152 @@ public:
 
     int Search(string name) {
         int idx = lower_bound(this->pictures.begin(), this->pictures.end(), name) - this->pictures.begin();
-        return (this->pictures[idx] == name && idx < this->pictures.size() ? idx : EOF);
+        return ((idx < this->pictures.size() && this->pictures[idx] == name) ? idx : EOF);
     }
 };
 
 class Computer {
 public:
-    Album top;
+    Album* top;
+    Album* cur;
+    Album* ser;
+    int cnt_a;
+    int cnt_p;
 
     Computer() {
-        this->top.name = "album";
+        Album* newAlbum = new Album("album");
+        this->top = newAlbum;
+        this->cur = this->top;
+        this->ser = NULL;
+    }
+
+    void Make(string name) {
+        for (int i = 0; i < this->cur->children.size(); i++) {
+            if (this->cur->children[i]->name == name) return;
+        }
+        Album* newAlbum = new Album(name);
+        newAlbum->parent = this->cur;
+        this->cur->children.push_back(newAlbum);
+        sort(this->cur->children.begin(), this->cur->children.end());
+        this->cur->size_alb++;
+    }
+
+    void Remove(string name) {
+        if (name == "-1") {
+            this->cnt_a = this->cur->children[0]->size_alb + 1;
+            this->cnt_p = this->cur->children[0]->size_pic;
+            Counting(this->cur->children[0]);
+            this->cur->children.erase(this->cur->children.begin());
+            cout << cnt_a << " " << cnt_p << "\n";
+            return;
+        }
+        else if (name == "0") {
+            this->cnt_a = this->cur->size_alb;
+            this->cnt_p = 0;
+            for (int i = 0; i < this->cur->children.size(); i++) {
+                Counting(this->cur->children[i]);
+            }
+            cout << cnt_a << " " << cnt_p << "\n";
+            return;
+        }
+        else if (name == "1") {
+            this->cnt_a = this->cur->children[this->cur->children.size() - 1]->size_alb + 1;
+            this->cnt_p = this->cur->children[this->cur->children.size() - 1]->size_pic;
+            Counting(this->cur->children[this->cur->children.size() - 1]);
+            this->cur->children.pop_back();
+            cout << cnt_a << " " << cnt_p << "\n";
+            return;
+        }
+        else {
+            for (int i = 0; i < this->cur->children.size(); i++) {
+                if (this->cur->children[i]->name == name) {
+                    this->cnt_a = this->cur->children[i]->size_alb + 1;
+                    this->cnt_p = this->cur->children[i]->size_pic;
+                    Counting(this->cur);
+                    this->cur->children.erase(this->cur->children.begin() + i);
+                    cout << cnt_a << " " << cnt_p << "\n";
+                    return;
+                }
+            }
+        }
+    }
+
+    void Counting(Album* ptr) {
+        for (int i = 0; i < ptr->children.size(); i++) {
+            if (ptr->children[i] != NULL) {
+                Counting(ptr->children[i]);
+                this->cnt_a += ptr->children[i]->size_alb;
+                this->cnt_p += ptr->children[i]->size_pic;
+            }
+        }
+    }
+
+    void Change(string name) {
+        if (name == "..") {
+            if (this->cur->name == "album") {
+                cout << this->cur->name << "\n";
+                return;
+            }
+            this->cur = this->cur->parent;
+        }
+        else if (name == "/") this->cur = this->top;
+        else {
+            Search(this->cur, name);
+            if (this->ser != NULL) this->cur = this->ser;
+            this->ser = NULL;
+        }
+        cout << this->cur->name << "\n";
+    }
+
+    void Search(Album* ptr, string name) {
+        if (ptr->name == name) {
+            this->ser = ptr;
+            return;
+        }
+        for (int i = 0; i < ptr->children.size(); i++) {
+            if (ptr->children[i] != NULL) {
+                if (ptr->children[i]->name == name) {
+                    this->ser = ptr->children[i];
+                    return;
+                }
+                Search(ptr->children[i], name);
+            }
+        }
     }
 
 
 };
-using namespace std;
 
 int main() {
+    int N;
+    string S, str;
+    Computer pc;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> S;
+        if (S == "mkalb") {
+            cin >> str;
+            pc.Make(str);
+        }
+        else if (S == "rmalb") {
+            cin >> str;
+            pc.Remove(str);
+        }
+        else if (S == "insert") {
+            cin >> str;
+            pc.cur->Insert(str);
+        }
+        else if (S == "delete") {
+            cin >> str;
+            pc.cur->Delete(str);
+        }
+        else if (S == "ca") {
+            cin >> str;
+            pc.Change(str);
+        }
+    }
 
     return 0;
 }
